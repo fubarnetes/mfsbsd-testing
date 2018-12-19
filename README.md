beba53b4ce02ab3cf451760cd23a44c151fb2fda
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2018-12-19 04:21:57 +0100

    Fixed upload and added SSH keys

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mfsbsd-testing
 Testing MFSBSD image
+
+# SSH
+The SSH Keys in the `keys` directory allow sshing into the mfsbsd image. *THESE KEYS SHOULD NOT BE USED ANYWHERE ELSE*.
--- build.sh
+++ build.sh
@@ -1,24 +1,56 @@
 #!/bin/sh
 
 ARCH=amd64
-FBSD_RELEASE="12.0-STABLE"
+FBSD_RELEASES="11.2-STABLE 12.0-STABLE"
+OWNER=fubarnetes
+REPO=mfsbsd-testing
 
 git submodule init
 git submodule update
 
-fetch -m http://ftp.freebsd.org/pub/FreeBSD/snapshots/${ARCH}/${FBSD_RELEASE}/kernel.txz
-fetch -m http://ftp.freebsd.org/pub/FreeBSD/snapshots/${ARCH}/${FBSD_RELEASE}/base.txz
-
 cp conf/* mfsbsd/conf/
+cat keys/*.pub >> mfsbsd/conf/authorized_keys
 
-cd mfsbsd
-make iso MFSROOT_MAXSIZE=1G BASE=..
+if [ -z $GITHUB_TOKEN ]; then
+        echo "GITHUB_TOKEN not set. Exiting."
+        exit 1
+fi
 
-[ -z $GITHUB_TOKEN ] && ( echo "GITHUB_TOKEN not set. Exiting." && exit 1 )
-[ -z $TAG ] && ( echo "TAG not set. Exiting." && exit 1 )
+if [ -z $TAG ]; then
+        echo "TAG not set. Exiting."
+        exit 1
+fi
 
-curl \
-	-H "Authorization: token $GITHUB_TOKEN" \
-	-H "Content-Type: $(file -b --mime-type mfsbsd-${FBSD_RELEASE}-${ARCH}.iso)" \
-	--data-binary @mfsbsd-${FBSD_RELEASE}-${ARCH}.iso \
-	"https://uploads.github.com/repos/fubarnetes/mfsbsd-testing/releases/${TAG}/assets?name=mfsbsd-${FBSD_RELEASE}-${ARCH}.iso"
+
+echo downloading distfiles...
+for FBSD_RELEASE in ${FBSD_RELEASES}; do
+        mkdir -p ${FBSD_RELEASE}
+        curl --progress-bar -C - \
+                -o ${FBSD_RELEASE}/kernel.txz \
+                http://ftp.freebsd.org/pub/FreeBSD/snapshots/${ARCH}/${FBSD_RELEASE}/kernel.txz
+        curl --progress-bar -C - \
+                -o ${FBSD_RELEASE}/base.txz \
+                http://ftp.freebsd.org/pub/FreeBSD/snapshots/${ARCH}/${FBSD_RELEASE}/base.txz
+done
+
+for FBSD_RELEASE in ${FBSD_RELEASES}; do
+        make -C mfsbsd iso RELEASE=${FBSD_RELEASE} MFSROOT_MAXSIZE=1G BASE=../${FBSD_RELEASE}
+done
+
+AUTHHDR="Authorization: token $GITHUB_TOKEN"
+# FIXME: check the auth hdr
+
+RELEASE_ID=$(curl -s -H "${AUTHHDR}" https://api.github.com/repos/${OWNER}/${REPO}/releases | jq -r '.[]|select(.tag_name=="'${TAG}'")|.id')
+UPLOAD_URL=$(curl -s -H "${AUTHHDR}" https://api.github.com/repos/${OWNER}/${REPO}/releases/${RELEASE_ID} | jq -r '.upload_url' | cut -d'{' -f1 )
+
+echo "uploading images..."
+for FBSD_RELEASE in ${FBSD_RELEASES}; do
+        ISOFILE=mfsbsd-${FBSD_RELEASE}-${ARCH}.iso
+        UPLOAD=$(curl \
+                --progress-bar \
+                -H "${AUTHHDR}" \
+                -H "Content-Type: $(file -b --mime-type mfsbsd/${ISOFILE})" \
+                -H "Accept: application/vnd.github.manifold-preview" \
+                --data-binary @mfsbsd/${ISOFILE} \
+                "${UPLOAD_URL}?name=${ISOFILE}")
+done
