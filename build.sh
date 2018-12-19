20d3d517f6ea06a993b8d2cbc9ef5385254180c9
Fabian Freyer <fabian.freyer@physik.tu-berlin.de>
2018-12-19 01:29:40 +0100

    github push

--- build.sh
+++ build.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-RELEASE="12.0-STABLE"
+ARCH=amd64
+FBSD_RELEASE="12.0-STABLE"
 
 git submodule init
 git submodule update
 
-fetch http://ftp.freebsd.org/pub/FreeBSD/snapshots/amd64/${RELEASE}/kernel.txz
-fetch http://ftp.freebsd.org/pub/FreeBSD/snapshots/amd64/${RELEASE}/base.txz
-fetch http://ftp.freebsd.org/pub/FreeBSD/snapshots/amd64/${RELEASE}/boot.txz
+fetch -m http://ftp.freebsd.org/pub/FreeBSD/snapshots/${ARCH}/${FBSD_RELEASE}/kernel.txz
+fetch -m http://ftp.freebsd.org/pub/FreeBSD/snapshots/${ARCH}/${FBSD_RELEASE}/base.txz
 
 cp conf/* mfsbsd/conf/
 
 cd mfsbsd
-make MFSROOT_MAXSIZE=1G BASE=..
+make iso MFSROOT_MAXSIZE=1G BASE=..
+
+[ -z $GITHUB_TOKEN ] && ( echo "GITHUB_TOKEN not set. Exiting." && exit 1 )
+[ -z $TAG ] && ( echo "TAG not set. Exiting." && exit 1 )
+
+curl \
+	-H "Authorization: token $GITHUB_TOKEN" \
+	-H "Content-Type: $(file -b --mime-type mfsbsd-${FBSD_RELEASE}-${ARCH}.iso)" \
+	--data-binary @mfsbsd-${FBSD_RELEASE}-${ARCH}.iso \
+	"https://uploads.github.com/repos/fubarnetes/mfsbsd-testing/releases/${TAG}/assets?name=mfsbsd-${FBSD_RELEASE}-${ARCH}.iso"
